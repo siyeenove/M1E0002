092a292df4254b365d82278095547b0d11ad8ebd
SIYEENOVE <mosiwi@outlook.com>
2025-11-07 19:08:31 +0800

    Adding python tutorials

---
+++ Micropython/Code/Python/6-7-main.py
@@ -0,0 +1,90 @@
+# Imports go at the top
+from microbit import *
+
+# Car I2C address
+i2cAddr = 0x29
+i2cBuf = bytearray([0x00, 0x00])
+
+# S1--S4 pins mode
+s1ToS4 = 15
+pwmMode = 1
+servoMode = 2
+
+# Servo type
+servo90  = 0
+servo180 = 1
+servo270 = 2
+pinS1 = 20    # S1 pin
+pinS2 = 21    # S2 pin
+pinS3 = 22    # S3 pin
+pinS4 = 23    # S4 pin
+
+# Set the S1--S4 pin mode
+# mode: 1 = PWM mode, 2 = servo mode
+def setPinMode(mode):
+    if mode == servoMode:          
+        i2cBuf[1] = servoMode 
+    elif mode == pwmMode:
+        i2cBuf[1] = pwmMode
+    else:
+        return
+    i2cBuf[0] = s1ToS4
+    i2c.write(i2cAddr, i2cBuf)
+
+# Set the Angle function of 90, 180 and 270 servo.
+# index: 20 = S1 pin, 21 = S2 pin, 22 = S3 pin, 23 = S4 pin
+# servoType: 0 = 90 servo, 1 = 180 servo, 2 = 270 servo
+# angle: 90 servo -> 0-90, 180 servo -> 0-180, 270 servo -> 0-270
+def setServo(index, servoType, angle):
+    angleMap = 0
+    if servoType == servo90:
+        # Map 0-90 to 50-200
+        angleMap = scale(angle, from_=(0, 90), to=(50, 200))
+    if servoType == servo180:
+        # Map 0-90 to 50-200
+        angleMap = scale(angle, from_=(0, 180), to=(50, 200))
+    if servoType == servo270:
+        # Map 0-90 to 50-200
+        angleMap = scale(angle, from_=(0, 270), to=(50, 200))
+        
+    if index == pinS1:
+        i2cBuf[0] = pinS1  # S1 pin
+    if index == pinS2:
+        i2cBuf[0] = pinS2  # S2 pin
+    if index == pinS3:
+        i2cBuf[0] = pinS3  # S3 pin
+    if index == pinS4:
+        i2cBuf[0] = pinS4  # S4 pin
+
+    i2cBuf[1] = angleMap
+    i2c.write(i2cAddr, i2cBuf)
+
+# Set the speed of 360 servo
+# index: 20 = S1 pin, 21 = S2 pin, 22 = S3 pin, 23 = S4 pin
+# speed: -100 to +100
+def setServo360(index, speed):
+    # Map -100 - 100 to 0 - 180
+    angle = scale(speed, from_=(-100, 100), to=(0, 180))
+    setServo(index, servo180, angle)
+    
+# Set the pins S1--S4 to servo mode
+setPinMode(servoMode)
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    # The 360-degree servo of the S1--S4 pins is forward 
+    # at the maximum speed
+    setServo360(pinS1, 100)
+    setServo360(pinS2, 100)
+    setServo360(pinS3, 100)
+    setServo360(pinS4, 100)
+    sleep(1000)
+    
+    # The 360-degree servo of the S1--S4 pins is backward 
+    # at the maximum speed
+    setServo360(pinS1, -100)
+    setServo360(pinS2, -100)
+    setServo360(pinS3, -100)
+    setServo360(pinS4, -100)
+    sleep(1000)
+
